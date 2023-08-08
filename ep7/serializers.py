2336bc9fc0bec3d552a953ca112e70376704114a
Abel A. Marquele <aamarquele@gmail.com>
2023-08-08 15:20:28 +0200

    Adicionar depth = 1 para ver toda informação da api

--- ep7/serializers.py
+++ ep7/serializers.py
@@ -6,108 +6,130 @@ class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = '__all__'
+        depth = 1
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        depth = 1
 
 class PessoaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pessoa
         fields = '__all__'
+        depth = 1
 
 class EntidadeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Entidade
         fields = '__all__'
+        depth = 1
 
 class EntidadePessoaSerializer(serializers.ModelSerializer):
     class Meta:
         model = EntidadePessoa
         fields = '__all__'
+        depth = 1
 
 class ProvinciaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Provincia
         fields = '__all__'
+        depth = 1
 
 class DistritoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Distrito
         fields = '__all__'
+        depth = 1
 
 class EstabelecimentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estabelecimento
         fields = '__all__'
+        depth = 1
 
 class TipoVinculacaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipoVinculacao
         fields = '__all__'
+        depth = 1
 
 class TipoEntradaSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipoEntrada
         fields = '__all__'
+        depth = 1
 
 class TipoPerdaSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipoPerda
         fields = '__all__'
+        depth = 1
 
 class CargoFuncaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = CargoFuncao
         fields = '__all__'
+        depth = 1
 
 class EspecialidadeMedicaSerializer(serializers.ModelSerializer):
     class Meta:
         model = EspecialidadeMedica
         fields = '__all__'
+        depth = 1
 
 class RegimeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Regime
         fields = '__all__'
+        depth = 1
 
 class NivelOcupacaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = NivelOcupacao
         fields = '__all__'
+        depth = 1
 
 class CarreiaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Carreia
         fields = '__all__'
+        depth = 1
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = '__all__'
+        depth = 1
 
 class EscalaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Escalao
         fields = '__all__'
+        depth = 1
 
 class SectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sector
         fields = '__all__'
+        depth = 1
 
 class FinanciamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Financiamento
         fields = '__all__'
+        depth = 1
 
 class EntradaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Entrada
         fields = '__all__'
+        depth = 1
 
 class SaidaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Saida
         fields = '__all__'
+        depth = 1
--- ep7project/settings.py
+++ ep7project/settings.py
@@ -40,14 +40,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
-    'ep7',
     'drf_yasg',
+    'corsheaders',
+
+    'ep7',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -75,6 +79,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ep7project.wsgi.application'
 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://localhost:3000",
+    "https://127.0.0.1:3000",
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
